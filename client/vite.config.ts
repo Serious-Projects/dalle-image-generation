864bc8e9992b5fe4e1f751d3ecd1a9cb649f1792
ayush-dedhia25 <ayushdedhia25@gmail.com>
2023-01-22 20:31:25 +0530

    fixed frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'https://github.com/Serious-Projects/dalle-image-generation/',
 	plugins: [react()],
 });
